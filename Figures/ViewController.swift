a4b00b7b281d50fad760f514a3d04d2b36d6d2b3
Art Karma <inste@icloud.com>
2019-04-25 13:33:21 +0300

    Second commit

--- Figures/ViewController.swift
+++ Figures/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var animator: UIDynamicAnimator!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +18,21 @@ class ViewController: UIViewController {
         let square = FiguresBuilder().setFrame(CGRect(x: 300, y: 210, width: 100, height: 100)).setColor(.green).builder()
         let diamond = FiguresBuilder().setFrame(CGRect(x: 10, y: 160, width: 100, height: 100)).setColor(.red).setCornerRadius(150).builder()
         let circle = FiguresBuilder().setFrame(CGRect(x: 150, y: 110, width: 100, height: 100)).setColor(.purple).setCornerRadius(50).builder()
+        let rectangle = FiguresBuilder().setFrame(CGRect(x: 0, y: 400, width: 600, height: 30)).setColor(.orange).builder()
         
         self.view.addSubview(square)
         self.view.addSubview(diamond)
         self.view.addSubview(circle)
-
+        self.view.addSubview(rectangle)
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        let gravity = UIGravityBehavior(items: [square, diamond, circle])
+        gravity.magnitude = 0.1
+        animator.addBehavior(gravity)
+        
+        let collision = UICollisionBehavior(items: [square, diamond, circle, rectangle])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
     }
 
 
